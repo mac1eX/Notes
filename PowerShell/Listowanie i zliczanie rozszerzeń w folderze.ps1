a7585a38d0f4db2824bc4bdfe55673a376fdac2a
mac1eX <65808933+mac1eX@users.noreply.github.com>
2020-06-11 22:55:56 +0200

    Update Listowanie i zliczanie rozszerzeń w folderze.ps1

--- PowerShell/Listowanie i zliczanie rozszerzeń w folderze.ps1
+++ PowerShell/Listowanie i zliczanie rozszerzeń w folderze.ps1
@@ -1,1 +1,5 @@
-Get-ChildItem -Recurse | Group-Object Extension -NoElement
+#Wylistowanie wszystkich rozszerzeń i ich zliczenie
+Get-ChildItem -Path c:\test\ -Recurse | Group-Object Extension -NoElement
+
+#Wykistowanie ścieżek do plików z konkretnym rozszerzeniem
+Get-ChildItem -Path C:\test\ -recurse -include *.php
