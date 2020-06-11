41d109e00dbfed766b0f46bdbf9dda97378951bd
mac1eX <65808933+mac1eX@users.noreply.github.com>
2020-06-11 23:24:35 +0200

    Update Wyszukiwanie roszerzeń.ps1

--- PowerShell/Wyszukiwanie roszerzeń.ps1
+++ PowerShell/Wyszukiwanie roszerzeń.ps1
@@ -1,5 +1,5 @@
 #Wylistowanie wszystkich rozszerzeń i ich zliczenie. Usunąć -Include aby zliczyło wszystkie rozszerzenia.
-Get-ChildItem -Path c:\test\ -Recurse -Include *.php | Group-Object Extension -NoElement
+Get-ChildItem -Path c:\test\ -Recurse -File -Include *.php | Group-Object Extension -NoElement
 
 #Wykistowanie ścieżek do plików z konkretnym rozszerzeniem
 Get-ChildItem -Path C:\test\ -recurse -include *.php
